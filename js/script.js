cce82412f6808b66fe02b3035d2829aa0414a306
keith-kit <keithkit@users.noreply.github.com>
2020-05-15 18:42:47 -0700

    Completed appendPageLinks. Remove HTML alterations. Started Search functionality.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
-
 //////////
 // Init //
 //////////
 
 // Select class Page
-const selectPage = document.getElementsByClassName('page')[0];
+const selectPage = document.querySelector('.page');
+// Select header
+const selectPageHeader = document.querySelector('.page-header');
 // Select container holding list students
 const selectUnorderedList = document.querySelector('.student-list');
 // Select listed students
-let selectListItem = selectUnorderedList.children;
+const selectListItem = selectUnorderedList.children;
 // Select input text box
 const selectInputTextName = document.querySelector('#inputTextName');
 // Select submit entry button
@@ -29,107 +22,120 @@ const maxItemsPerPage = 10;
 // Functions //
 ///////////////
 
-// Random number generator for random gender and avatar
-function randNum(num) {
-   return Math.floor(Math.random() * num);
+// Create search form
+function appendSearchBar() {
+// Create div to hold search contents
+const div = document.createElement('div');
+div.className = 'search-form';
+
+// Create input search textbox
+const searchBar = document.createElement('input');
+searchBar.className = 'search-element';
+searchBar.type = 'text';
+div.appendChild(searchBar);
+
+// Create execute search button
+const searchButton = document.createElement('button');
+searchButton.className = 'search-element';
+searchButton.textContent = 'Search for student';
+div.appendChild(searchButton);
+
+// Add a reset button
+
+// Append div into Page Header
+selectPageHeader.appendChild(div);
 }
 
+// Search student functionality
+function searchStudent() {
+   // Get string in textbox
+   // Hide all students
+   // Using search method, go through student list
+   // If search is true make hidden false
+}
+
+// Hide all students
 function hideAllListItems(bool) {
    for (let i = 0; i < selectListItem.length; i++) {
       selectListItem[i].hidden = bool;
    }
 }
 
-// Functionality to show specific results on page
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+// Show filtered students on page
 const showPage = (list, page) => {
    // Hide all entries
    hideAllListItems(true);
-   // Current active page.  Always 1.
-   const currentPage = 1;
    // startingIndex
-   let startIndex = (page - currentPage) * maxItemsPerPage;
-   console.log(startIndex);
+   let startIndex = (page - 1) * maxItemsPerPage;
    // endingIndex subtracted by one to accomodate for computer counting
    const endIndex = (page * maxItemsPerPage) - 1;
-   console.log(endIndex);
-   // loop over items in list parameter
+   // loop over items in list
    for (let i = 0; i < list.length; i++) {
       // if index >= firstItem && index <= lastItem
       if (i >= startIndex && i <= endIndex) {
-         //showItems
-         console.log(`showing: [${i}]`);
+         // showItems
          list[i].hidden = false;
       }
    }
 }
 
-// Functionality for bottom page links
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+// Create bottom pagination links
 const appendPageLinks = (list) => {
    // Pages needed by divinding total list items by max items per page
    const pages = Math.trunc(list.length / maxItemsPerPage) + 1;
    
-   // Create a div and give it the 'pagination' class, append it to the .page div
-   // Possibly needs DIV or 'DIV'
+   // Create a div and give it the 'pagination' class, embed an unordered list in it
    const div = document.createElement('div');
    div.className = 'pagination';
-
-   // Unordered List Div
    const ul = document.createElement('ul');
 
-   // for every page, add li and a tags with the page number text
-   for (let i = 0; i < pages; i++) {
-      // Possibly needs LI or 'LI'
+   // For every page, add li and a tags with the page number text
+   for (let i = 1; i <= pages; i++) {
+      // Create List Item and a Hyperlink with the text displaying the page number
       const li = document.createElement('li');
-      // li.textContent = i;   
-      
-      // Create hyperlink
       const a = document.createElement('a');
       a.textContent = i;
-      // a.href = showPage(selectListItem, i);
-
+      a.addEventListener('click', (e) => {
+         showPage(selectListItem, a.textContent);
+         // Loop over pagination links
+         const selectPagination = document.querySelector('.pagination');
+         const children = selectPagination.firstElementChild.childNodes;
+         // Go through all pagination links and make only the selected "active" class
+         for (i = 0; i < children.length; i++) {
+            const child = children[i].firstChild;
+            child.className = '';
+            if (e.target.textContent === child.textContent) {
+               e.target.className = 'active';
+            }
+         } 
+      })
       li.appendChild(a);
-      ul.appendChild(li);
-      div.appendChild(ul);
+      ul.appendChild(li);     
    }
-
-   // add an event listener to each a tag.  when clicked call showPage()
-
-   // loop over pagination links to remove active class from all links
-
-   // add active class to the link that was just clicked.  you can identifiy that clicked link using event.target.
+   div.appendChild(ul);
    selectPage.appendChild(div);
 }
 
+appendSearchBar();
 appendPageLinks(selectListItem);
 
-///////////////
-// Listeners //
-///////////////
+/////////////////////////
+// Debugging functions //
+/////////////////////////
+
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+******************************************/
+   
+// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+// Random number generator for random gender and avatar
+function randNum(num) {
+   return Math.floor(Math.random() * num);
+}
 
 document.addEventListener('click', (event) => {
-   if (event.target.id == 'buttonTop10') {
-      for (let i = 10; i < selectListItem.length; i +=1) {
-         selectListItem[i].hidden = true;
-      }
-   }
    if (event.target.id == 'buttonReset') {
       hideAllListItems(false);
    }
@@ -142,11 +148,7 @@ document.addEventListener('click', (event) => {
       
       // Vanity function that converts single digit numbers into double digits
       function makeDoubleDigit(val) {
-         if (val < 10) { 
-            return newVal = '0' + val;
-         } else { 
-            return val;
-         }   
+         if (val < 10) { return newVal = '0' + val; } else { return val; }   
       }
       // Auto generate details like Date, gender, and portrait
       const customDate = new Date();
@@ -154,7 +156,7 @@ document.addEventListener('click', (event) => {
       const dd = makeDoubleDigit(customDate.getDate());
       const yy = customDate.getFullYear().toString().substr(-2);
       let gender = '';
-      // New if-then method I'm practicing
+      // I learned this If-then tecnique from: https://stackoverflow.com/questions/8860654/javascript-single-line-if-statement-best-syntax-this-alternative
       (randNum(2)) ? gender = 'women' : gender = 'men';
       let customerPortrait = `https://randomuser.me/api/portraits/thumb/${gender}/${randNum(99)}.jpg`;
       newLi.innerHTML = 
@@ -176,7 +178,4 @@ document.addEventListener('click', (event) => {
 
    // Remove last list item listener
       if (event.target.id == 'buttonPopBottom') { selectUnorderedList.removeChild(selectUnorderedList.lastElementChild); }
-   
-   //Update List of Students
-   selectListItem = document.querySelectorAll('li');
 });
